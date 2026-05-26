64493f79ed67f54118cb2102aa109ab4315320ff
AllenP <Allenjamfa@gmail.com>
2026-05-26 02:06:26 -0700

    feat: add get_grid NOAA step 1 function

---
+++ signals/weather.py
@@ -0,0 +1,25 @@
+import os 
+import requests 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NOAA_BASE_URL = "https://api.weather.gov"
+
+NOAA_HEADERS = {
+    "User-Agent": "kalshi-bot/1.0 (Allenjamfa@gmail.com)",
+    "Accept": "application/geo+json"
+}
+
+
+def get_grid(lat, lon):
+    url = f"{NOAA_BASE_URL}/points/{lat},{lon}"
+    response = requests.get(url, headers=NOAA_HEADERS)
+    data = response.json()
+    props = data["properties"]
+    return {
+        "gridId": props["gridId"],
+        "gridX": props["gridX"],
+        "gridY": props["gridY"],
+        "forecast_url": props["forecast"],
+    }
